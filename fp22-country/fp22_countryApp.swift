0c5d40121d3611403c56b17ff5221deb192d42f7
VincentZhou11 <zhouvincent11@gmail.com>
2022-04-05 18:17:32 -0400

    Initial Commit

---
+++ fp22-country/fp22_countryApp.swift
@@ -0,0 +1,17 @@
+//
+//  fp22_countryApp.swift
+//  fp22-country
+//
+//  Created by Vincent Zhou on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct fp22_countryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
