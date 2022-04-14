0e114625166135694b78717167188fb494a8f7c1
VincentZhou11 <zhouvincent11@gmail.com>
2022-04-13 21:03:52 -0400

    Put structs for Ai response into another file

---
+++ fp22-country/AI/AIStructs.swift
@@ -0,0 +1,19 @@
+//
+//  AIStructs.swift
+//  fp22-country
+//
+//  Created by Vincent Zhou on 4/13/22.
+//
+
+import Foundation
+
+struct AIResponse: Decodable {
+    let id: String
+    let object: String
+    let created: Int
+    let model: String
+    let choices: [Choices]
+}
+struct Choices: Decodable {
+    let text: String
+}
